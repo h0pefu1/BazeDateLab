45f114cdcda32e7a5432fd5e48341b8738e0e462
h0pefu1 <malix2k17@gmail.com>
2022-09-11 21:17:46 +0300

    Added project

---
+++ DataBaseLab/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DataBaseLab.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
